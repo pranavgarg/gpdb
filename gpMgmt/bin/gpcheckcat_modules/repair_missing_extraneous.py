b7ee0b898d09db8db1fe59e8d137f153de43681d
Nikhil Kak <nkak@pivotal.io>
2016-05-24 10:12:26 -0700

    wip 2

--- gpMgmt/bin/gpcheckcat_modules/repair_missing_extraneous.py
+++ gpMgmt/bin/gpcheckcat_modules/repair_missing_extraneous.py
@@ -3,34 +3,48 @@ from gppylib.gpcatalog import *
 
 class RepairMissingExtraneous:
 
-    def __init__(self):
-        self.delete_sql = "delete from {0} where {1}={2};"
+    def __init__(self, catalog_table_obj, db_name, repair_script_path, issues, pk_name):
+        self._delete_sql = "delete from {0} where {1}={2};"
+        self._catalog_table_obj = catalog_table_obj
+        self._table_name = self._catalog_table_obj.getTableName()
+        self._db_name = db_name
+        self._repair_script_path = repair_script_path
+        self._issues = issues
+        self._pk_name = pk_name
 
     def create_missing_repair_sql(self):
         return "repair script"
 
-    def create_extra_repair_sql(self, catalog_table, entries, fkeylist):
-        oids_to_remove = set()
+    def get_extra_repair_sql_contents(self):
 
-        if entries:
-            for entry in entries:
-                oids_to_remove.add(entry[0])
+        if not self._issues:
+            return
 
-        if catalog_table == "pg_class":
-            oid_column = "oid"
-        elif fkeylist:
-            for fkey in fkeylist:
-                if fkey.getPkeyTableName() == "pg_class":
-                    oid_column = fkey.getColumns()[0]
-
-        repair_sql_contents = []
-
-        for oid_to_remove in oids_to_remove:
-            repair_sql_contents.append(self.delete_sql.format(catalog_table,
-                                                              oid_column,
-                                                              oid_to_remove))
+        oids_to_remove = {issue[0] for issue in self._issues if issue[0]}
+        repair_sql_contents = [self._delete_sql.format(self._catalog_table_obj.getTableName(),
+                                                       self._pk_name, oid_to_remove)
+                               for oid_to_remove in oids_to_remove]
 
         return repair_sql_contents
 
+    def create_repair_sql(self,  repair_type_prefix, timestamp):
 
+        contents = self.get_extra_repair_sql_contents()
+        sql_file_name = '{0}.{1}_{2}.{3}.sql'.format(self.db_name, repair_type_prefix,
+                                                     self._table_name,timestamp)
+        repair_script_path = '{0}/{1}'.format(self._repair_script_path, sql_file_name)
+
+        sql_file_obj = None
+        try:
+            sql_file_obj = open(repair_script_path, 'w')
+            sql_file_obj.writelines(contents)
+        except Exception, e:
+            logger.fatal('Unable to create file "%s": %s' % (repair_script_path, str(e)))
+            sys.exit(1)
+        finally:
+            if sql_file_obj:
+                sql_file_obj.close()
+
+        description = '\necho "something extra"\n'
+        return description, sql_file_name
 
--- gpMgmt/bin/gppylib/test/unit/test_unit_repair_missing_extraneous.py
+++ gpMgmt/bin/gppylib/test/unit/test_unit_repair_missing_extraneous.py
@@ -2,26 +2,25 @@ from mock import *
 
 from gp_unittest import *
 from gpcheckcat_modules.repair_missing_extraneous import RepairMissingExtraneous
-from gppylib.gpcatalog import GPCatalogTableForeignKey
 
 class RepairMissingExtraneousTestCase(GpTestCase):
     def setUp(self):
         self.subject = RepairMissingExtraneous()
 
     def test_create_repair_sql_returns_sql(self):
-        entries =\
-        [(49401, 'cmax', 49401, '{2}'), (49401, 'cmin', 49401, '{2}'), (49401, 'ctid', 49401, '{2}'), (49401, 'gp_segment_id', 49401, '{2}'), (49401, 'tableoid', 49401, '{2}'), (49401, 'xmax', 49401, '{2}'), (49401, 'xmin', 49401, '{2}')]
+        entries = [(49401, 'cmax', 49401, '{2}'),
+                   (49401, 'cmin', 49401, '{2}'),
+                   (49401, 'ctid', 49401, '{2}'),
+                   (49401, 'gp_segment_id', 49401, '{2}'),
+                   (49401, 'tableoid', 49401, '{2}'),
+                   (49401, 'xmax', 49401, '{2}'),
+                   (49401, 'xmin', 49401, '{2}')]
 
-        """"[GPCatalogTableForeignKey: pg_attribute; col: [u'attrelid']; , o
-            GPCatalogTableForeignKey: pg_attribute; col: [u'atttypid']; ]"""
-        fkObj1 = GPCatalogTableForeignKey(tname="pg_attribute",cols=["attrelid"],
-                                         pktablename="pg_class",pkey="oid")
-        fkObj2 = GPCatalogTableForeignKey(tname="pg_attribute",cols=["atttypid"],
-                                         pktablename="pg_type",pkey="oid")
-        fklist = [fkObj1, fkObj2]
-        repair_sql_contents = self.subject.create_extra_repair_sql("pg_attribute",
+        catalog_table_mock = Mock(spec=['getTableName'])
+        catalog_table_mock.getTableName.return_value = "pg_attribute"
+        repair_sql_contents = self.subject.create_extra_repair_sql(catalog_table_mock,
                                                                    entries,
-                                                                   fklist)
+                                                                   "attrelid")
 
 
         self.assertEqual(len(repair_sql_contents), 1)
