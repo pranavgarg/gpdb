1a4430dffc39c1cd841181a03823dd022c263b50
Marbin Tan <mtan@pivotal.io>
2016-06-01 17:28:35 -0700

    Create extra repair portion
    * create delete sql file and bash file to repair corrupted extra entries

--- gpMgmt/bin/gpcheckcat_modules/repair_missing_extraneous.py
+++ gpMgmt/bin/gpcheckcat_modules/repair_missing_extraneous.py
@@ -1,55 +1,32 @@
 #!/usr/bin/env python
 from gppylib.gpcatalog import *
 
-
-class IssueInfo: # TODO: Replace me later
-
-    def __init__(self, catalog_name, primary_key, primary_key_value):
-        self.catalog_name = catalog_name
-        self.primary_key = primary_key
-        self.primary_key_value = primary_key_value
-
-    def get_delete_sql(self):
-        return "delete from {0} where {1}={2};".format(self.catalog_name,
-                                                       self.primary_key,
-                                                       self.primary_key_value)
-
-
 class RepairMissingExtraneous:
 
-    def __init__(self, catalog_table_obj, db_name, repair_script_path, issues, pk_name, segmentinfo_map):
-        self._catalog_table_obj = catalog_table_obj
-        self._table_name = self._catalog_table_obj.getTableName()
-        self._db_name = db_name
-        self._repair_script_path = repair_script_path
+    def __init__(self, catalog_table_obj,  issues, pk_name):
+        self._table_name = catalog_table_obj.getTableName()
         self._issues = issues
         if pk_name is None: # None is crazy
             pk_name = 'oid'
         self._pk_name = pk_name
-        self._segmentinfo_map = segmentinfo_map
 
-    def create_missing_repair_sql(self):
-        return "repair script"
+    def get_delete_sql(self, primary_key_value):
+        # TODO: add begin and commit
+        return "set allow_system_table_mods='dml';"\
+                "delete from {0} where {1}={2};".format(self._table_name,
+                                                       self._pk_name,
+                                                       primary_key_value)
 
-    def get_tentative_segments(self):
-        # We assume that segmentid will always be the last one
-        return {int(issue[-1].strip('{}')) for issue in self._issues if issue[0]}
-
-    def get_issuesinfo(self):
-        for segid in segmentids:
-            myprint(GV.report_cfg[segid])
-        return
-
-    def get_extra_repair_sql_contents(self):
-
+    def get_extra_or_missing_oids_in_segments(self):
         if not self._issues:
             return
 
-        # TODO: We need to create a sql file per segment
-        oids_to_remove = {issue[0] for issue in self._issues if issue[0]}
-        repair_sql_contents = ["set allow_system_table_mods='dml';"]
-        repair_sql_contents += [self._delete_sql.format(self._catalog_table_obj.getTableName(),
-                                                            self._pk_name, oid_to_remove)
-                                   for oid_to_remove in oids_to_remove]
+        oids_to_segment_mapping = {}
+        for issue in self._issues:
+            seg_ids = issue[-1].strip('{}').split(',')
+            if not oids_to_segment_mapping.has_key(issue[0]):
+                oids_to_segment_mapping[issue[0]] = set()
+            oids_to_segment_mapping[issue[0]].update(int(seg_id) for seg_id in seg_ids)
 
-        return repair_sql_contents
+        return oids_to_segment_mapping
+
