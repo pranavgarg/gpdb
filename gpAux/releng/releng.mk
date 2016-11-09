aeaaef8aa0e9dc3f8dbf29010fff20ad66bc436f
Jesse Zhang and Shoaib Lari <pair+jzhang+slari@pivotal.io>
2016-11-09 15:50:34 -0800

    Revert "Clone gpaddon repository to gpAux/addon directory."
    This reverts commit 2757e2cb548920d7f2a71106715107c39c05cbb3.

--- gpAux/releng/releng.mk
+++ gpAux/releng/releng.mk
@@ -177,7 +177,6 @@ sync_tools: opt_write_test /opt/releng/apache-ant
 
 	wget -O - https://github.com/greenplum-db/gpos/releases/download/v1.146/bin_gpos_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
 	wget -O - https://github.com/greenplum-db/gporca/releases/download/v1.684/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
-	git clone git@github.com:greenplum-db/gpaddon.git $(BLD_TOP)/gpAux/addon
 
 clean_tools: opt_write_test
 	@cd releng/make/dependencies; \
